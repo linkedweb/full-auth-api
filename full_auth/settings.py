74dc9a68639bbdffd7b3eb7567e2183f65bdff1b
Bryan Brkic <bryanbrkic@linkedweb.ca>
2023-05-21 17:31:16 -0400

    add custom media location

--- full_auth/settings.py
+++ full_auth/settings.py
@@ -171,9 +171,10 @@ else:
     }
     AWS_DEFAULT_ACL = 'public-read'
     AWS_LOCATION = 'static'
+    AWS_MEDIA_LOCATION = 'media'
     AWS_S3_CUSTOM_DOMAIN = getenv('AWS_S3_CUSTOM_DOMAIN')
     STORAGES = {
-        'default': {'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'},
+        'default': {'BACKEND': 'custom_storages.CustomS3Boto3Storage'},
         'staticfiles': {'BACKEND': 'storages.backends.s3boto3.S3StaticStorage'}
     }
 
